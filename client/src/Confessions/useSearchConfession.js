d4c27dbde8acae4bd5288e450e9a79dc651b46cd
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-09-05 20:31:37 -0700

    search function

--- client/src/Confessions/useSearchConfession.js
+++ client/src/Confessions/useSearchConfession.js
@@ -2,17 +2,26 @@ import { useEffect, useState } from 'react';
 import axios from '../Axios';
 
 export default function useSearchConfession(pageNumber, searchQuery) {
-    const [confessions, setConfessions] = useState([])
+    const [confessions, setConfessions] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [hasMore, setHasMore] = useState(false)
     useEffect(() => {
         axios({
         method: 'GET',
         url: `/confessions/search`,
         params: {page: pageNumber-1, q: searchQuery}
         }).then(res => {
-            console.log(res.data)
-            setConfessions(res.data)
+            setConfessions([...confessions, ...res.data]);
+            setHasMore(res.data.length > 0)
+            setLoading(false)
+        }).catch(e => {
+            if(axios.isCancel(e)) return
+            setError(true)
         })
 
     }, [pageNumber, searchQuery])
-  return confessions
+
+    console.log(confessions)
+  return {confessions, loading, error, hasMore}
 }
--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -4,42 +4,32 @@ import './Pages.css'
 import { useParams } from 'react-router-dom';
 import CategoryLinks from '../components/CategoryLinks/CategoryLinks';
 import ConfessionList from '../components/ConfessionList/ConfessionList';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import useSearchConfession from '../Confessions/useSearchConfession';
 
 const Search = ({searchParams}) => {
-    const [test, setTest] = useState(0);
-    const [res, setRes] = useState([])
     const [searchString, setSearchString] = useState(searchParams);
-    // useEffect(() => {
-        
-    //     const getConfessions = async () => {
-    //         try {
-    //             const res = await axios.get(`/confessions/search/?search=${searchParams}`);
-    //             setData(res.data)
-    //         } catch(err) {
-    //             throw err
-    //         }
-    //     }
+    const [pageNumber, setPageNumber] = useState(1);
+    const observer = useRef()
+    const {
+      confessions,
+      hasMore,
+      loading,
+      error
+    } = useSearchConfession(pageNumber, searchString);
 
-    //     getConfessions()
+    const nextPage = () => {
+      setPageNumber(prevPageNum => {
+        return prevPageNum + 1
+      });
+    }
 
-    // }, [searchParams]);
-
-    const data = useSearchConfession(1, searchParams)
-
-    useEffect(() => {
-      setRes(data)
-    
-    }, [data])
-    
-
-
+    console.log(confessions)
   return (
     <>
         <CategoryLinks />
 
-        <ConfessionList confessions={res} />
+        <ConfessionList data={{confessions, loading, pageNumber, hasMore}} nextPage={nextPage}/>
 
     </>
   )
